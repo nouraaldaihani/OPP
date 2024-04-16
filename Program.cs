2582f490637cb5019a0da8643853839b41002b71
KFH <KFH@LAPTOP-TRN-136>
2024-04-16 09:21:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+var student = new Student("Noura", "EE", 4.0);
+
+student.PrintInformation();
+public class Student
+{
+
+    public string Name { get; set; }
+    public string Major { get; set; }
+    public double GPA { get; set; }
+
+    public Student(string name, string major, double gpa)
+
+    {
+        Name = name;
+        Major = major;
+        GPA = gpa;
+
+    }
+
+    public void PrintInformation()
+    {
+        Console.WriteLine($"Name : {Name}");
+        Console.WriteLine($"Major : {Major}");
+        Console.WriteLine($"GPA : {GPA}");
+
+    }
+}
